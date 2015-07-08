5b54711525a8ec5f871b0705a2b796c26c91cde9
Hans-Philipp Brachvogel <brachvo@abibuilder3.Informatik.Uni-Tuebingen.De>
2015-07-08 14:27:13 +0200

    updated image names and added a build script to translate all Dockerfiles to images

---
+++ build_images.sh
@@ -0,0 +1,5 @@
+docker build -t centos_6_ball_bare centos_6_ball_bare/
+docker build -t centos_6_ball_minimal centos_6_ball_minimal/
+
+docker build -t ubuntu_1404_ball_bare ubuntu_1404_ball_bare/
+docker build -t ubuntu_1404_ball_minimal ubuntu_1404_ball_minimal/
